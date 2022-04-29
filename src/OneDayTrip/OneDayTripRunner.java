eee7e524fd3506ff1ba7743b4e5a3c4c9a481346
yunus pektaş <Yunuspektas@protonmail.com>
2022-04-29 21:38:45 +0300

    Delete src/OneDayTrip directory

--- src/OneDayTrip/OneDayTripRunner.java
+++
@@ -1,9 +0,0 @@
-package OneDayTrip;
-
-import OneDayTrip.Restaurant.IslemYap;
-
-public class OneDayTripRunner {
-    public static void main(String[] args) {
-        IslemYap.SecımYap();
-    }
-}
--- src/OneDayTrip/README.md
+++
@@ -1,2 +0,0 @@
-Think about that, just one day trip, different counyry
-Lets code that scenario .. 
